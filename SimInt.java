c1055581f9e3439c2138a798759dc96f7e4fb231
Alapaka Prasanthi <85541983+prasanthi1409@users.noreply.github.com>
2023-12-28 10:20:36 +0530

    Add files via upload

---
+++ SimInt.java
@@ -0,0 +1,11 @@
+class SimInt 
+{
+	public static void main(String[] args) 
+	{
+		int p = 500;
+		int t = 5;
+		int r = 3;
+		double res = (p*t*r)/100;
+		System.out.println("For"+p+"for"+t+"months"+"SimInt is:"+res);
+	}
+}
