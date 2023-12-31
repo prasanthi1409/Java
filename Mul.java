50b0662350eaae03b2d7f7a989786294bdc07226
Alapaka Prasanthi <85541983+prasanthi1409@users.noreply.github.com>
2023-12-31 11:15:44 +0530

    Add files via upload

---
+++ Mul.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Mul
+{
+	public static void main(String[] args) 
+	{
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		System.out.println("Enter a num: ");
+		for(i=1;i<=n;i++){
+			int a = 6;
+			int n = 0;
+			n=a*i;
+		System.out.println(a+ "X" +i+ "=" +n);
+		}
+	}
+}
