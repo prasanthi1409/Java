f5a0ff431fd0b57d03ce533d7724f9ba93a47203
Alapaka Prasanthi <85541983+prasanthi1409@users.noreply.github.com>
2024-01-05 10:14:24 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,17 @@
+class Pattern 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Enter a num: ");
+		int n=5;
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
