94dbd05021ca8f271fc213fdd76a8324ece49cf9
Alapaka Prasanthi <85541983+prasanthi1409@users.noreply.github.com>
2024-01-08 19:28:17 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,18 @@
+class  Circle
+{
+	int radius;
+	Circle()
+	{
+		radius=r;
+	}
+}
+Class CircleCal
+{
+	public static void main(String[] args) 
+	{
+		Circle c=new Circle(5);
+		System.out.println(c.radius);
+		c.radius=25;
+		System.out.println(c.radius);
+	}
+}
